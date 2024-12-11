bf43661e005801b129d20b2e346c6efeff2fe0ce
Ilham <222212756@stis.ac.id>
2024-12-11 15:16:50 +0700

    fix: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\FormPengajuan;
+use App\Models\Komponen;
+use App\Models\KRO;
+use App\Models\SubKomponen;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +25,13 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(AkunBelanjaSeeder::class);
+        $this->call(FormPengajuanSeeder::class);
+        $this->call(KegiatanSeeder::class);
+        $this->call(KomponenSeeder::class);
+        $this->call(SubKomponenSeeder::class);
+        $this->call(KROSeeder::class);
+        $this->call(OutputSeeder::class);
+        $this->call(PegawaiSeeder::class);
+        // $this->call(ROSeeder::class);
     }
 }
